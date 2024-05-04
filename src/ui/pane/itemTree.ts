1237bcbaf737fc210aa129aa32ee4472329b6d6c
alima-webdev <alima.webdev@gmail.com>
2024-05-04 12:32:01 -0400

    .

--- src/ui/pane/itemTree.ts
+++ src/ui/pane/itemTree.ts
@@ -33,7 +33,7 @@ export class ReviewModule {
             properties: {
                 type: "text/css",
                 rel: "stylesheet",
-                href: `chrome://${config.addonRef}/content/styles/styles.css`,
+                href: `chrome://${config.addonRef}/content/styles/styles.css?${Date.now()}`,
                 id: `${config.addonRef}-stylesheet`,
             },
         });
